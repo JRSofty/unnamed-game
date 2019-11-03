c9e462e84dca94c193e36ee6bdb1365272c5f42a
Jason Reed <jrsofty@gmail.com>
2019-11-03 22:20:30 +0100

    Corrects placement of Texture in TextureFactory interface

--- game-lib-wrapper/src/main/java/com/github/jrsofty/game/lib/wrapper/textures/TextureFactory.java
+++ game-lib-wrapper/src/main/java/com/github/jrsofty/game/lib/wrapper/textures/TextureFactory.java
@@ -1,7 +1,5 @@
 package com.github.jrsofty.game.lib.wrapper.textures;
 
-import com.jrsofty.github.game.api.Texture;
-
 public interface TextureFactory {
     Texture createTexture(String resourceName);
 }
