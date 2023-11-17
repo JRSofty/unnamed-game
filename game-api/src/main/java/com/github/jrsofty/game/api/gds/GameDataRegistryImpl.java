816db2d69e351b24309b70f818308604c252f245
Jason Reed <jrsofty@gmail.com>
2023-11-17 22:57:39 +0100

    Implements GameDataRegistry

---
+++ game-api/src/main/java/com/github/jrsofty/game/api/gds/GameDataRegistryImpl.java
@@ -0,0 +1,24 @@
+package com.github.jrsofty.game.api.gds;
+
+import java.util.LinkedHashMap;
+
+public class GameDataRegistryImpl implements GameDataRegistry {
+
+    private final LinkedHashMap<String, GameDataEntry<?>> internalMap = new LinkedHashMap<>();
+
+    @Override
+    public void addEntry(final String entryName, final GameDataEntry<?> entry) {
+        this.internalMap.put(entryName, entry);
+    }
+
+    @Override
+    public GameDataEntry<?> getEntry(final String entryName) {
+        return this.internalMap.get(entryName);
+    }
+
+    @Override
+    public boolean containsEntry(final String entryName) {
+        return this.internalMap.containsKey(entryName);
+    }
+
+}
