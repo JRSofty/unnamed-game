d1e9e7c25be74763deeb950e10bff83e5a50ff3e
Jason Reed <jrsofty@gmail.com>
2023-11-17 23:59:46 +0100

    Corrects factory pattern

--- game-api/src/main/java/com/github/jrsofty/game/api/gds/GameDataRegistryFactory.java
+++ game-api/src/main/java/com/github/jrsofty/game/api/gds/GameDataRegistryFactory.java
@@ -2,20 +2,18 @@ package com.github.jrsofty.game.api.gds;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public abstract class GameDataRegistryFactory {
+
     private static final GameDataRegistryFactory INSTANCE = new GameDataRegistryFactoryImpl();
 
     public static GameDataRegistryFactory getDefaultFactory() {
         return GameDataRegistryFactory.INSTANCE;
     }
 
-    abstract GameDataRegistry createRegistry(String registryName);
-
-    abstract GameDataRegistry getRegistry(String registryName);
-
-    abstract void deleteRegistry(String registryName);
-
-    static class GameDataRegistryFactoryImpl extends GameDataRegistryFactory {
+    public static class GameDataRegistryFactoryImpl extends GameDataRegistryFactory {
         private final HashMap<String, GameDataRegistry> registries = new HashMap<>();
 
         @Override
@@ -42,6 +40,25 @@ public abstract class GameDataRegistryFactory {
             this.registries.remove(registryName);
         }
 
+        @Override
+        public String serializeRegistry(final String registryName) throws JsonProcessingException {
+            if (!this.registries.containsKey(registryName)) {
+                throw new RuntimeException("Registry does not exist");
+            }
+            final ObjectMapper mapper = new ObjectMapper();
+            final String output = mapper.writeValueAsString(this.registries.get(registryName));
+            return output;
+
+        }
+
     }
+
+    public abstract GameDataRegistry createRegistry(String registryName);
+
+    public abstract GameDataRegistry getRegistry(String registryName);
+
+    public abstract void deleteRegistry(String registryName);
+
+    public abstract String serializeRegistry(String registryName) throws JsonProcessingException;
 
 }
