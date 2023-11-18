126865dc1167b914a37d8c7f75856bbe7852920b
Jason Reed <jrsofty@gmail.com>
2023-11-18 21:21:52 +0100

    Changes FileReader interface

--- game-api/src/main/java/com/github/jrsofty/game/api/io/FileReader.java
+++ game-api/src/main/java/com/github/jrsofty/game/api/io/FileReader.java
@@ -1,9 +1,12 @@
 package com.github.jrsofty.game.api.io;
 
 import java.io.IOException;
+import java.util.Set;
 
 public interface FileReader {
     String getPath();
 
     byte[] readFileData(String fullPath) throws IOException;
+
+    Set<String> listFilesInPath();
 }
